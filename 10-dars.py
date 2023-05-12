66ebe925cebc0e6e2384e71927c999033f3d417b
Ali Svoy <131866997+alisvoy@users.noreply.github.com>
2023-05-12 15:08:18 +0500

    Create 10-dars.py

---
+++ 10-dars.py
@@ -0,0 +1,67 @@
+# # # SintaxError
+# print("Hello World!")
+# print('Hello World!')
+# print('Assalom alaykum!')
+
+# # IndentationError
+#   print("Hello World!")
+
+# print("O'ngacha sanaymiz")
+# for n in range(1, 11):
+# print(n)
+
+
+# # mantiqiy xato ... dastur tugri ishlaydi 
+# son = 50
+# if son <= 0:
+#     print("Musbat son")
+# else:
+#     print("Manfiy son")
+
+# # TypeError
+# son = input("Istalgan son kiriting: ")
+# print(f"{son} ning kvadrati {son**2} ga teng")
+
+# #NameError
+# print("Hello World!")
+# mevalar = ['olma','uzum','nok','anor','anjir']
+# for meva in mevalar:
+#     print(meva)
+
+# # ValueError
+# son = int(input("Istalgan son kiriting: "))
+# if son >= 0:
+#     print("Musbat son")
+# else:
+#     print("Manfiy son")
+
+# #IndexError
+# mevalar = ['olma','anor','uzum']
+# print(mevalar[3])
+
+# # ZeroDivisionError
+# x, y = 50, 50
+# z = 250/(x-y)
+
+# # Mantiqiy xatolar
+# radius = 5
+# pi = 3.14
+# aylana_yuzi = pi*radius**2
+# print(aylana_yuzi)
+
+# son = float(input("Istalgan son kiriting: "))
+# ildiz = son**(1/2)
+# print(f"{son} ning ildizi {ildiz} ga teng")
+
+# mevalar = ['olma','uzum','nok','anor','anjir']
+# for meva in mevalar:
+#     print(meva)
+# print("Dastur tugadi")
+
+
+# b = list(range(11))
+
+# for n in range(1, 11):
+#     for i in mevalar:
+#         print(i)
+#     print(f'son {n+1}')
